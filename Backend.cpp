6d850165dfbf3f276cc7b52e473579e5a2471c4b
Manjitha Marasinghe <2017manjitha@gmail.com>
2022-09-18 14:36:36 +0530

    reverse temporary to temp

--- Backend.cpp
+++ Backend.cpp
@@ -48,7 +48,7 @@ public:
         // assigning the all the pointers in the array to NULL
             head[i] = NULL; 
             gateArr[i] = gates[i];
-            Vertice temporary;
+            Vertice temp;
             temp.index = i;
             temp.gateType = gateArr[i];
             vertexArr.push_back(temp);
